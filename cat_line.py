dc4cffda959ce620899e08c0a7b88727ea024024
kannbo <113285773+kannbo@users.noreply.github.com>
2024-03-20 18:16:52 +0900

    Update cat_line.py

--- cat_line.py
+++ cat_line.py
@@ -1,5 +1,5 @@
 import scratchattach as scratch3,time,random,requests
-session = scratch3.login("kannbo", {{password}}) # 自分のパスワード
+session = scratch3.login("kannbo", {{PASSWORD}}) # 自分のパスワード
 conn = session.connect_cloud("980998475") # project_id
 NAME={}
 ii=0
