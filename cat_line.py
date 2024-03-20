6b5ff769436c5e67b81e9b2f63e23fcde902b63a
kannbo <113285773+kannbo@users.noreply.github.com>
2024-03-20 18:11:55 +0900

    Create cat_line.py

---
+++ cat_line.py
@@ -0,0 +1,110 @@
+import scratchattach as scratch3,time,random,requests
+session = scratch3.login("kannbo", {{password}}) # 自分のパスワード
+conn = session.connect_cloud("980998475") # project_id
+NAME={}
+ii=0
+debug=True #デバッグ以外False!!
+for i in """-
+_
+1
+2
+3
+4
+5
+6
+7
+8
+9
+0
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+k
+l
+n
+m
+o
+p
+q
+r
+s
+t
+u
+v
+w
+x
+y
+z
+Z
+Y
+X
+W
+V
+U
+T
+S
+R
+Q
+P
+O
+N
+M
+L
+K
+J
+I
+H
+G
+F
+E
+D
+C
+B
+A""".split("\n"):
+    ii+=1
+    if ii%10==0:
+        ii+=1
+    NAME[str(ii)]=i
+print(NAME)
+name=""
+while True:
+    name=""
+    time.sleep(random.randint(1,4))
+    variables = scratch3.get_cloud("980998475")
+    #print(scratch3.get_cloud("980998475"))
+    #print(variables["UserName"],end=" ")
+    for i in variables["UserName"].split("0"):
+        
+        if i=="":
+            pass
+        else:
+            name=name+NAME[i]
+    if debug:
+        print(name)
+    try:
+        getdata2=scratch3.get_user(name).follower_count()
+        conn.set_var("follower",str(getdata2))
+        getdata3=scratch3.get_user(name).following_count()
+        conn.set_var("follow",str(getdata3))
+        conn.set_var("error","0")
+        getdata1=requests.get(f"https://api.scratch.mit.edu/users/{name}/messages/count/").json()
+        conn.set_var("message",str(getdata1["count"]))
+        time.sleep(1)
+        UNAME=variables["UserName"]
+        conn.set_var("UserName",UNAME)
+    except:
+        conn.set_var("error","1")
+        print("error")
+        conn.set_var("follow","0")
+        conn.set_var("follower","0")
+        conn.set_var("message","0")
+        UNAME=variables["UserName"]
+        conn.set_var("UserName",UNAME)
+
