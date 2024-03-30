8253b631e5313ecb9ab0a95e1a29e0bfd0d2a52f
kannbo <113285773+kannbo@users.noreply.github.com>
2024-03-30 16:53:56 +0900

    Update web.py

--- web.py
+++ web.py
@@ -2,8 +2,8 @@ from bottle import route, run, template
 import subprocess
 subprocess.Popen(["python3","cat_line.py"])
 
-@route('/hello/<name>')
+@route('/')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('<b>Hello {{name}}</b>!', name="world")
 
 run(host='0.0.0.0', port=10000)
